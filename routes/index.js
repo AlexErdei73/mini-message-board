32cf4d7ab516cd4f95c64df50ea3e684a1fdb11a
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-09-10 09:30:00 +0100

    Add date-fns formatDistnceNow to index view

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var datefns = require("date-fns");
 
 //messages to show
 const messages = [
@@ -17,12 +18,32 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "The Odin Message Board", messages: messages });
+  // We use date-fns formatDistanceToNow function to show the added field in index view
+  const formatDistanceToNow = datefns.formatDistanceToNow;
+  res.render("index", {
+    title: "The Odin Message Board",
+    messages: messages,
+    formatDistanceToNow: formatDistanceToNow,
+  });
 });
 
 /* GET new message page */
 router.get("/new", function (req, res, next) {
   res.render("form", { title: "New message" });
+});
+
+/* POST new message page */
+router.post("/new", function (req, res, next) {
+  const { text, user } = req.body;
+  if (!text || !user) {
+    return res.render("form", { title: "Please add some text and user!" });
+  }
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
